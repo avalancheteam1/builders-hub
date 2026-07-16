6cfdaea7e4c5d0a4dbe7e20f96368afb0647993f
Ashutosh Tripathi <ashucoder9@gmail.com>
2026-07-16 17:08:36 +0400

    feat(search): remove "search by tags" option

--- components/search.tsx
+++ components/search.tsx
@@ -1,20 +1,16 @@
 "use client";
 
-import { useState } from "react";
 import { liteClient } from "algoliasearch/lite";
 import { useDocsSearch } from "fumadocs-core/search/client";
 import {
   SearchDialog,
   SearchDialogClose,
   SearchDialogContent,
-  SearchDialogFooter,
   SearchDialogHeader,
   SearchDialogIcon,
   SearchDialogInput,
   SearchDialogList,
   SearchDialogOverlay,
-  TagsList,
-  TagsListItem,
   type SharedProps,
 } from "fumadocs-ui/components/dialog/search";
 
@@ -23,27 +19,19 @@ import {
 //
 // Composed from fumadocs' native dialog primitives instead of the shipped
 // <AlgoliaSearchDialog> because 16.0.15 renders that component's footer as a
-// sibling of the dialog content, leaking the tag chips onto the page while
-// the dialog is closed (the orama variant nests it correctly).
+// sibling of the dialog content, leaking it onto the page while the dialog
+// is closed (the orama variant nests it correctly).
 const appId = "0T4ZBDJ3AF";
 const apiKey = "9b74c8a3bba6e59a00209193be3eb63a";
 const indexName = "builder-hub";
 
 const client = liteClient(appId, apiKey);
 
-const tagItems = [
-  { name: "Docs", value: "docs" },
-  { name: "Academy", value: "academy" },
-  { name: "Integrations & Guides", value: "ig" },
-];
-
 export default function CustomSearchDialog(props: SharedProps) {
-  const [tag, setTag] = useState<string | undefined>(undefined);
   const { search, setSearch, query } = useDocsSearch({
     type: "algolia",
     client,
     indexName,
-    tag,
   });
 
   return (
@@ -61,15 +49,6 @@ export default function CustomSearchDialog(props: SharedProps) {
           <SearchDialogClose />
         </SearchDialogHeader>
         <SearchDialogList items={query.data !== "empty" ? query.data : null} />
-        <SearchDialogFooter>
-          <TagsList tag={tag} onTagChange={setTag} allowClear>
-            {tagItems.map((item) => (
-              <TagsListItem key={item.value} value={item.value}>
-                {item.name}
-              </TagsListItem>
-            ))}
-          </TagsList>
-        </SearchDialogFooter>
       </SearchDialogContent>
     </SearchDialog>
   );
